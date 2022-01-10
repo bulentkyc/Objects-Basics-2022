1b2025b2534f77d02560e9baa4e5efe04d7dedab
BULENT KAYICI <bulent@Bulents-Mac-mini.fritz.box>
2022-01-10 14:57:56 +0100

    v0.2

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ console.log('myPen.color: ', myPen.color);
 myPen.myKeyName = 'yellow';
 console.log('myPen: ', myPen);
 
-myPen[myKeyName] = prompt('Tell me your favorite color')
+//myPen[myKeyName] = prompt('Tell me your favorite color')
 console.log('myPen: ', myPen);
 
 //Mistake, variable names should be used without quotes
@@ -75,8 +75,13 @@ console.log('myPen: ', myPen);
 //Syntax:
 //JSON.stringify(object);
 
+//JSON.parse
+//Syntax:
+//JSON.parse(object);
 //////////////////////////////////////////////////////////
 //Delete
+
+
 let strMyPen = JSON.stringify(myPen);
 console.log('String myPen: ', strMyPen);
 console.log(typeof strMyPen);
@@ -89,6 +94,11 @@ console.log('myPen: ', myPen);
 
 delete myPen[myKeyName];
 console.log('myPen: ', myPen);
+console.log('strMyPen: ', strMyPen);
+console.log('toStr: ', myPen.toString());
+
+let newPen = JSON.parse(strMyPen);
+console.log('newPen: ', newPen);
 //////////////////////////////////////////////////////////
 let myFirstObj = {};
 
@@ -123,12 +133,33 @@ console.log(myAuto);
 
 
 //////////////////////////////////////////////////////////
-let x = 123;
+//Dynamic parameter names
+let curColor = 'white';
 
-const increaseRandomly = (initialValue) => {
-    return `${initialValue + Math.ceil(Math.random()*10)}`;
+let criteria = prompt('Please tell me the most important future of a mobile phone for you')
+let criteriaVal = prompt('Please tell me the value of the future')
+
+let myHandy = {
+    capacity: '512gb',
+    color: curColor,
+    [criteria]: criteriaVal
 }
 
-x = increaseRandomly(x)
-console.log(x);
-console.log(typeof x);
+console.log(myHandy);
+//////////////////////////////////////////////////////////
+
+let name = prompt('name:');
+let age = prompt('age:');
+let email = prompt('email:');
+
+let person = {
+    name,
+    age,
+    email
+}
+
+console.log(person);
+
+//////////////////////////////////////////////////////////
+
+console.log({name,age})
